275b8f711870f962629e3eb7ae2dde2432367e5c
Roshith Balendran <roshithbalendran@gmail.com>
2018-11-27 16:49:05 +0530

    Added iOS Fundamentals Xcode Project.
    Added iOS Fundamentals Xcode Project.

---
+++ iOS-Fundamentals/iOS-Fundamentals/QuickAccessTemplate.swift
@@ -0,0 +1,14 @@
+//
+//  QuickAccessTemplate.swift
+//  iOS-Fundamentals
+//
+//  Created by Roshith Balendran on 27/11/18.
+//  Copyright © 2018 Roshith Balendran. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuickAccessTemplate {
+    
+}
