cf85b12340fb99e4930765c97a4eccd8e46da200
Roshith Balendran <roshithbalendran@hq.microobjects.net>
2019-01-14 17:57:45 +0530

    Local Notification implemented in Home Screen.

--- iOS-Fundamentals/iOS-Fundamentals/AppDelegate.swift
+++ iOS-Fundamentals/iOS-Fundamentals/AppDelegate.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate  {
+    
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        let options: UNAuthorizationOptions = [UNAuthorizationOptions.sound, UNAuthorizationOptions.alert]
+        notificationCenter.requestAuthorization(options: options) { (granted, error) in
+            if error != nil {
+                print(error)
+            }
+        }
+        notificationCenter.delegate = self
+        
         return true
     }
 
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ iOS-Fundamentals/iOS-Fundamentals/Splash Screen/Animated Splash Screen/AnimatedSplashScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AnimatedSplashScreenViewController.swift
+//  iOS-Fundamentals
+//
+//  Created by Roshith Balendran on 27/11/18.
+//  Copyright © 2018 Roshith Balendran. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedSplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
